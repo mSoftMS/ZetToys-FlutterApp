c3f35025758cc4c9815ca79eaa839631210023de
Jakub Stępień <jakub.stepien2905@gmail.com>
2024-03-03 13:51:28 +0100

    feat: initial view UI

--- lib/view/initial_view.dart
+++ lib/view/initial_view.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_joy_ble/l10n/l10n.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:gap/gap.dart';
+import 'package:get/utils.dart';
 
 class InitialView extends StatelessWidget {
   const InitialView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  Widget build(BuildContext context) => Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              context.l10n.appTitle,
+              style: context.textTheme.headlineMedium,
+            ),
+            const Gap(15),
+            const SpinKitWave(color: Colors.blue),
+          ],
+        ),
+      );
 }
